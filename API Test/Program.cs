137731882a5e8a65aac8b9aeed9e7fd57698464c
Jeremy Lee <Taka86@hotmail.co.uk>
2020-02-25 16:17:14 +0000

    jhklfdbgsbd.jh

---
+++ API Test/Program.cs
@@ -0,0 +1,67 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace API_Test
+{
+   
+
+    class Program
+    {
+        private const string URL = "https://api.publicapis.org/entries?category=animals";
+
+        public static async void GetProjects()
+		{
+
+
+            using (HttpClient client = new HttpClient())
+            {
+                new MediaTypeWithQualityHeaderValue("application/json");
+                HttpResponseMessage response = client.GetAsync(URL).Result;  // Blocking call! Program will wait here until a response is received or a timeout occurs.
+
+                if (response.IsSuccessStatusCode)
+                {
+                    Console.WriteLine("Success");
+                    string responseBody = await response.Content.ReadAsStringAsync();
+                    Console.WriteLine(responseBody);
+                    var API = JsonConvert.DeserializeObject<RootObject>(responseBody);
+                    Console.WriteLine(API);
+                    var cats = API.Results;
+                    Console.WriteLine(cats);
+                    
+                    foreach (var cat in API.Results.OrderBy( c => c.Description).Take(5))
+                    {
+                        Console.WriteLine(cat.Description);
+                    }
+                    foreach (var cat2 in cats.Where((c) => c.Cors == "unknown"))
+                    {
+                        Console.WriteLine(cat2.Api + " " + cat2.Description + " " + cat2.Cors);
+                    }
+                    var cat3 = cats.Count((c) => c.Cors == "unknown");
+                    Console.WriteLine(cat3);
+                    
+                }
+                else
+                {
+                    Console.WriteLine("{0} ({1})", (int)response.StatusCode, response.ReasonPhrase);
+                }
+                Console.WriteLine("Hello World!");
+
+            }
+        }
+	
+	static void Main(string[] args)
+        {
+			//Console.WriteLine("Hello World!");
+			GetProjects();
+			
+		}
+    }
+}
---
+++ API Test/Result.cs
@@ -0,0 +1,45 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace API_Test
+{
+    public class Result
+    {
+
+
+        [JsonProperty("API")]
+        public string Api { get; set; }
+
+        [JsonProperty("Description")]
+        public string Description { get; set; }
+
+        [JsonProperty("Auth")]
+        public string Auth { get; set; }
+
+        [JsonProperty("HTTPS")]
+        public bool Https { get; set; }
+
+        [JsonProperty("Cors")]
+        public string Cors { get; set; }
+
+        [JsonProperty("Link")]
+        public Uri Link { get; set; }
+
+        [JsonProperty("Category")]
+        public string Category { get; set; }
+    }
+       
+       
+    
+        public class RootObject
+        {
+            public bool isError { get; set; }
+            public int Status { get; set; }
+            public string Message { get; set; }
+
+        [JsonProperty("entries")]
+        public List<Result> Results { get; set; }
+        }
+}
